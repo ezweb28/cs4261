07586cd70f564ef5bbdc051002d5636038bac8a5
ezweb28 <ezweb28@gmail.com>
2019-01-29 12:43:40 -0500

    add independent todo page

---
+++ src/app/todo/todo.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo, TodoService } from './../services/todo.service';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.page.html',
+  styleUrls: ['./todo.page.scss'],
+})
+export class TodoPage implements OnInit {
+  todos: Todo[];
+
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit() {
+    this.todoService.getTodos().subscribe(res => {
+      this.todos = res;
+    });
+  }
+
+  remove(item) {
+    this.todoService.removeTodo(item.id);
+  }
+
+}
