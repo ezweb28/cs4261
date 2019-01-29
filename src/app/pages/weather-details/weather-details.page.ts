a2f38e6ade41c9db5d127cd868fdeefe416a2d9a
Malik Hayes <hayesmalik96@gmail.com>
2019-01-29 18:18:56 -0500

    OMDB Api implementation

---
+++ src/app/pages/weather-details/weather-details.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-details',
+  templateUrl: './weather-details.page.html',
+  styleUrls: ['./weather-details.page.scss'],
+})
+export class WeatherDetailsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
