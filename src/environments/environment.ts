ce65d4b46bd5e992b8e8d2b4c5a79e2f67ee18ad
ezweb28 <ezweb28@gmail.com>
2019-01-29 01:37:50 -0500

    Switch to new Ionic 4.9, and implemented Todo list that is used Firebase DB

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAVgr8tgszhYgE7cXdjkBcXeyb0gDT6jIQ",
+    authDomain: "ionicapp-ee7f9.firebaseapp.com",
+    databaseURL: "https://ionicapp-ee7f9.firebaseio.com",
+    projectId: "ionicapp-ee7f9",
+    storageBucket: "ionicapp-ee7f9.appspot.com",
+    messagingSenderId: "139768935636"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
