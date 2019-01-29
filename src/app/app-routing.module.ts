59d3b7f7cc1b2efc37dbbbdcff4b14813137d8b7
ezweb28 <ezweb28@gmail.com>
2019-01-29 15:50:34 -0500

    login works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'details', loadChildren: './pages/todo-details/todo-details.module#TodoDetailsPageModule' },
   { path: 'details/:id', loadChildren: './pages/todo-details/todo-details.module#TodoDetailsPageModule' },
+  { path: 'todo', loadChildren: './todo/todo.module#TodoPageModule' },
 ];
 
 @NgModule({
